dda9d7ee99bb7f63d7708479b1ca2423a5e7927a
liuguanyu <lgy573@126.com>
2016-03-09 11:47:24 +0800

    -

---
+++ urls.js
@@ -0,0 +1,14 @@
+module.exports = [
+    "http://bj.lianjia.com/ershoufang/shilibao/l2a4/",
+    "http://bj.lianjia.com/ershoufang/shilibao/pg2l2a4/",
+    "http://bj.5i5j.com/exchange/shilibao/r2",
+    "http://bj.5i5j.com/exchange/shilibao/r2n2",
+    "http://bj.5i5j.com/exchange/shilibao/r2n3",
+    "http://bj.5i5j.com/exchange/shilibao/r2n4",
+    "http://bj.5i5j.com/exchange/shilibao/r2n5",
+    "http://bj.5i5j.com/exchange/shilibao/r2n6",
+    "http://bj.5i5j.com/exchange/shilibao/r2n7",
+    "http://bj.5i5j.com/exchange/shilibao/r2n8",
+    "http://bj.5i5j.com/exchange/shilibao/r2n9",
+    "http://bj.5i5j.com/exchange/shilibao/r2n10"
+];
